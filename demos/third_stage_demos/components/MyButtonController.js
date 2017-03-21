ca58ab3ba5ebaf8e3890a5466b23e2d56646620c
wujiank <wujiank@yonyou.com>
2017-03-21 15:38:01 +0800

    新增flux框架简单示例

---
+++ demos/third_stage_demos/components/MyButtonController.js
@@ -0,0 +1,40 @@
+var React = require('react');
+var ListStore = require('../store/ListStore');
+var ButtonActions = require('../action/ButtonActions');
+var MyButton = require('./MyButton');
+
+var MyButtonController = React.createClass({
+  getInitialState: function () {
+    return {
+      items: ListStore.getAll()
+    };
+  },
+
+  componentDidMount: function() {
+    ListStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function() {
+    ListStore.removeChangeListener(this._onChange);
+  },
+
+  _onChange: function () {
+    this.setState({
+      items: ListStore.getAll()
+    });
+  },
+
+  createNewItem: function (event) {
+    ButtonActions.addNewItem('new item');
+  },
+
+  render: function() {
+    return <MyButton
+      items={this.state.items}
+      onClick={this.createNewItem}
+    />;
+  }
+
+});
+
+module.exports = MyButtonController;
---
+++ demos/third_stage_demos/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by magicwager on 2017/3/16.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var MyButtonController = require('./components/MyButtonController');
+
+ReactDOM.render(
+    <MyButtonController/>,
+    document.querySelector('#example')
+);
